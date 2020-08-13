8e1ffb249c660edd2230afb1aedb4b99c27dbfe8
Hudlle <68740062+Hudlle@users.noreply.github.com>
2020-08-14 00:40:54 +0200

    setup file

--- setup.py
+++ setup.py
@@ -1,34 +1,85 @@
 import pyautogui
 import time
+import simplejson as json
 
-def setup(fields_to_scan):
-    x_inv = []
-    y_inv = []
+def setup():
+    inv = [[] for _ in range(36)]
+    inv_y = []
+    inv_x = []
     counter = 1
 
-    for each in range(fields_to_scan):
-        time.sleep(3)
+    answer =  input("""Welcome to the setup!
+To start you have to make sure that you have the default controls in Minecraft unlocked. Otherwise the script wont work.
+Now you just can type 'start' and you will recieve further instructions.\n > """)
+    if answer.lower() == "start":
+        print("""For the scanning you will have to hover over a certain pattern of your inventory.
+First the most left 4 inventory slots then the whole first row. Here a pattern.""")
+        time.sleep(5)
+        print("<-------><-------><-------><-------><-------><-------><-------><-------><-------><------->")
+        print("|       ||       ||       ||       ||       ||       ||       ||       ||       ||       |")
+        print("|  1/5  ||   6   ||   7   ||   8   ||   9   ||   10  ||   11  ||   12  ||   13  ||   14  |")
+        print("|       ||       ||       ||       ||       ||       ||       ||       ||       ||       |")
+        print("<-------><-------><-------><-------><-------><-------><-------><-------><-------><------->")
+        print("|       ||       ||       ||       ||       ||       ||       ||       ||       ||       |")
+        print("|   2   ||       ||       ||       ||       ||       ||       ||       ||       ||       |")
+        print("|       ||       ||       ||       ||       ||       ||       ||       ||       ||       |")
+        print("<-------><-------><-------><-------><-------><-------><-------><-------><-------><------->")
+        print("|       ||       ||       ||       ||       ||       ||       ||       ||       ||       |")
+        print("|   3   ||       ||       ||       ||       ||       ||       ||       ||       ||       |")
+        print("|       ||       ||       ||       ||       ||       ||       ||       ||       ||       |")
+        print("<-------><-------><-------><-------><-------><-------><-------><-------><-------><------->")
+        print("<-------><-------><-------><-------><-------><-------><-------><-------><-------><------->")
+        print("|       ||       ||       ||       ||       ||       ||       ||       ||       ||       |")
+        print("|   4   ||       ||       ||       ||       ||       ||       ||       ||       ||       |")
+        print("|       ||       ||       ||       ||       ||       ||       ||       ||       ||       |")
+        print("<-------><-------><-------><-------><-------><-------><-------><-------><-------><------->")
+        answer = input("""If you are ready type 'ready' and the scanning will beginn in 3 seconds. After you typed ready jump in Minecraft and hover over the first slot.
+The script will show you when to move so you can just relax. \n > """)
+        if answer.lower() == "ready":
+            for i in range(3):
+                i += 1
+                print(str(i))
+                time.sleep(1)
+            print("Go!")
+        else:
+            exit()
+    else:
+        exit()
+
+    time.sleep(5)
+    for i in range(4):
         x, y = pyautogui.position()
-        x_inv.append(str(x))
-        x_inv.append(",")
-        y_inv.append(str(y))
-        y_inv.append(",")
-        print("#" + str(counter) + " Done! Move on.")
+        for i in range(9):
+            inv_y.append(y)
+        pyautogui.write("etMove", interval=0.1)
+        pyautogui.press("enter")
+        pyautogui.write("e")
+        time.sleep(3)
+
+    for i in range(9):
+        x, y = pyautogui.position()
+        inv_x.append(x)
+        pyautogui.write("etMove", interval=0.1)
+        pyautogui.press("enter")
+        pyautogui.write("e")
+        time.sleep(3)
+
+    counter = 0
+    x_counter = 0
+
+    for i in range(36):
+        inv[counter].append(inv_x[x_counter])
+        inv[counter].append(inv_y[counter])
+        x_counter += 1
         counter += 1
 
-    x_inv.pop()
-    y_inv.pop()
+        if x_counter >= 9:
+            x_counter = 0
 
-    with open("xdata.txt", "w") as file:
-        for i in x_inv:
-            file.write(i)
-    with open("ydata.txt", "w") as file:
-        for i in y_inv:
-            file.write(i)
+    with open("data.txt", "w") as file:
+        json.dump(inv, file)
 
-def main():
-    print("How many fields of your inventory do you want to scan? (all inventory slots are 36) :")
-    fields_to_scan = input(">")
-    setup(fields_to_scan)
+    print("Okay the scanning is finished. Bye :)")
+    time.sleep(1)
 
-main()
+setup()
